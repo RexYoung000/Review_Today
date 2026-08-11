41bd34c8e3288d6e164259569ad269796d3f2e31
RexYoung000 <RexYoung000@users.noreply.github.com>
2026-08-11 22:59:23 +0800

    Initial Commit

---
+++ Review_Today/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Review_Today
+//
+//  Created by Rex Young on 2026/8/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Review_Today/Review_TodayApp.swift
@@ -0,0 +1,17 @@
+//
+//  Review_TodayApp.swift
+//  Review_Today
+//
+//  Created by Rex Young on 2026/8/11.
+//
+
+import SwiftUI
+
+@main
+struct Review_TodayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
